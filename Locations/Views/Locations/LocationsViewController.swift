745f26c52e6c1358eb9fb01c4962e6154b114eb2
Arnosaurus <arneoldenhave@gmail.com>
2023-04-17 14:10:33 +0200

    added Collection+extenions

--- Locations/Views/Locations/LocationsViewController.swift
+++ Locations/Views/Locations/LocationsViewController.swift
@@ -119,10 +119,3 @@ extension LocationViewController {
         return cell
     }                                                                                                                                               
 }
-
-extension Collection {
-    /// Returns the element at the specified index if it is within bounds, otherwise nil.
-    subscript (safe index: Index) -> Element? {
-        return indices.contains(index) ? self[index] : nil
-    }
-}
